31754bae021f7d3f914bf4a6e0f7385f3ff19c4e
humam dwikat <=>
2021-10-19 17:14:26 +0300

    HTML file

---
+++ home.js
@@ -0,0 +1,14 @@
+function newphoto(){
+var imge=document.createElement('img');
+var div=document.getElementById('div-flex');
+imge.src="img/img7.png"  ;
+div.appendChild(imge);
+}
+
+
+function newphoto(){
+    var imge=document.createElement('img');
+    var div=document.getElementById('div-flex');
+    imge.src="img/img8.jfif"  ;
+    div.appendChild(imge);
+    }
